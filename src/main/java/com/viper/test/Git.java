86b604170da20744963d4055fef6c3016a6d1e63
Viper <57839761+viper0531@users.noreply.github.com>
2022-05-09 21:10:04 +0800

    pull commit

--- src/main/java/com/viper/test/Git.java
+++ src/main/java/com/viper/test/Git.java
@@ -9,5 +9,6 @@ public class Git {
         System.out.println("mater test");
         System.out.println("hotfix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
